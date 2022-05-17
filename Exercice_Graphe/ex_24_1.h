6986167774ba5d81b1db7a64089af7e9cc67c8cb
O'Vide <56994268+O-VideGribouille@users.noreply.github.com>
2022-05-17 17:02:03 +0200

    last commit

---
+++ Exercice_Graphe/ex_24_1.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <cassert>
+
+
+using namespace std;
+
+typedef struct s_sommet {
+
+	int n_sommet;
+	int n_id;
+	int n_v; //signifie qu'aucun sommet n'a été visité
+
+};
+
+typedef struct s_reseau {
+
+	s_sommet* tab_sommet; //tableau 1 dimension
+
+	char** tab_matrice; //valeur de 0 à 1, tableau à 2 dimensions
+
+	int n; //noeud réseau
+	int m; // cable qui relie deux noeuds
+	
+
+}s_reseau;
+
